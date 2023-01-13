c6a7b87ae77a65634ad48719e18b84bb6b41a92a
lihaiming <lihaiming@bytedance.com>
2023-01-13 15:15:47 +0800

    yangyang

---
+++ yangyang/server.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file        server.cpp
+ * @date        Sep 18, 2022
+ * @author      lihaiming
+ * @maintainer  lihaiming
+ * @brief       参照网上提供的思路，实现羊了个羊算法
+*/
+
+// 定义一个布局数组
+
+int generate() {
+    return 0;
+}
+
+int main() {
+    return 0;
+}
